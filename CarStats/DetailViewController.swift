a47ecb0b4cffdf71d1540a5d80fec15396c9b771
Theresa Hoyt <theresammcgovern@gmail.com>
2016-05-08 15:32:06 -0400

    Details being set and data being saved.

--- CarStats/DetailViewController.swift
+++ CarStats/DetailViewController.swift
@@ -8,18 +8,24 @@
 
 import UIKit
 
+extension UITableView {
+    func indexPathForView (view : UIView) -> NSIndexPath? {
+        let location = view.convertPoint(CGPointZero, toView:self)
+        return indexPathForRowAtPoint(location)
+    }
+}
 class DetailViewController: UITableViewController {
     
-    var text: String?
-    var titleAndLabel: String?
     
+    var text: String = ""
+    var fileName: String = ""
     @IBOutlet weak var detailDescriptionLabel: UILabel!
     
     let color = [UIColor(red: 28/255, green: 173/255, blue: 0/255, alpha: 1.0),
                  UIColor(red: 239/255, green: 183/255, blue: 0/255, alpha: 1.0),
                  UIColor(red: 255/255, green: 0/255, blue: 4/255, alpha: 1.0),
                  UIColor(red: 252/255, green: 247/255, blue: 143/255, alpha: 1.0)
-                ]
+    ]
     
     var CarStat: [String] = []
     var detailItem: AnyObject? {
@@ -28,7 +34,7 @@ class DetailViewController: UITableViewController {
             self.configureView()
         }
     }
-
+    
     
     func configureView() {
         // Update the user interface for the detail item.
@@ -38,11 +44,12 @@ class DetailViewController: UITableViewController {
             }
         }
     }
-
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
         self.configureView()
+        
     }
     
     
@@ -52,69 +59,106 @@ class DetailViewController: UITableViewController {
         
     }
     
-
-//    override func tableView(tableView: UITableView, accessoryButtonTappedForRowWithIndexPath indexPath: NSIndexPath) {
     
-   override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+    //    override func tableView(tableView: UITableView, accessoryButtonTappedForRowWithIndexPath indexPath: NSIndexPath) {
+    
+    override func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return 1
     }
-
-   override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
     
-    let Cell1 = tableView.dequeueReusableCellWithIdentifier("Cell1", forIndexPath: indexPath) as? customDetailCell
+    override func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        
+        
+        let Cell1 = tableView.dequeueReusableCellWithIdentifier("Cell1", forIndexPath: indexPath) as? customDetailCell
         Cell1!.textLabel!.font = UIFont(name: "Copperplate-Bold", size: 15)
-
-    
+        
+        
         switch(self.title){
         case "Gas"?:
             Cell1!.backgroundColor = color[0]
-             Cell1!.customView.backgroundColor = color[0]
-            titleAndLabel = "Gas/"
+            Cell1!.customView.backgroundColor = color[0]
+            fileName = "Gas.rtf"
             
         case "Oil"?:
             Cell1!.backgroundColor = color[1]
             Cell1!.customView.backgroundColor = color[1]
-            titleAndLabel = "Oil/"
+            
         case "Tires"?:
             Cell1!.backgroundColor = color[2]
             Cell1!.customView.backgroundColor = color[2]
-            titleAndLabel = "Tires/"
+            
         case "Inspection"?:
             Cell1!.backgroundColor = color[3]
             Cell1!.customView.backgroundColor = color[3]
-            titleAndLabel = "Inspection/"
+            
         default:
             print("default")
-        break
+            break
         }
+
         Cell1!.customTitle?.text = CarStat[indexPath.section]
         Cell1!.customViewTextField?.keyboardType = .DecimalPad
-        text = Cell1!.customViewTextField?.text
-        titleAndLabel = titleAndLabel! + "\(CarStat[indexPath.section])"
+        
+        if(Cell1!.customViewTextField.window != "nil"){
+            
+        }
+        
         return Cell1!
     }
 
     @IBAction func SaveInput(sender: AnyObject) {
-        writeToUserDefaults(titleAndLabel!)
-     
+
+        let value = sender as! UIButton
+        let indexPath = self.tableView.indexPathForView(value)!
+        
+        let cell = tableView.cellForRowAtIndexPath(indexPath) as? customDetailCell
+        
+        print(cell?.customTitle?.text!)
+        var input = cell?.customViewTextField?.text!
+        print(input)
+        
+        if(fileName.containsString("Gas")){
+            if(cell!.customTitle?.text! == "Insert Mileage"){
+                text = "mileage " + input!
+            }
+            else if(cell!.customTitle?.text! == "Insert Mileage"){
+                text = "price"  + input!
+            }
+            
+        }
+        print(text)
+        writeToFile(fileName, text: text)
+        cell!.customDetail.text = input
+        cell!.customViewTextField?.text! = ""
+  
+        cell!.customView.setNeedsDisplay()
+        input = ""
     }
+    
+
+    
     @IBAction func CancelInput(sender: AnyObject) {
         text = ""
     }
-    func writeToUserDefaults(titleAndLabel: String){
-        let defaults = NSUserDefaults.standardUserDefaults()
-        defaults.setObject(titleAndLabel, forKey: text!)
+    
+    func writeToFile(fileName: String, text: String){
+
+        let findFile = getDirectory().stringByAppendingPathComponent(fileName)
+        
+        do {
+            try text.writeToFile(findFile, atomically: true, encoding: NSUTF8StringEncoding)
+        } catch {
+            // failed to write file – bad permissions, bad filename, missing permissions, or more likely it can't be converted to the encoding
+        }
+        
         
     }
-    
-    func getData(name: String) -> String?{
-        let prefs = NSUserDefaults.standardUserDefaults()
-        if let details = prefs.stringForKey(titleAndLabel!){
-            return details as? String
-        }
-        return nil
+    func getDirectory() -> NSString{
+        let documentsPath = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let documentsDirectory = documentsPath[0]
+        return documentsDirectory
     }
-
+    
 }
 
 
